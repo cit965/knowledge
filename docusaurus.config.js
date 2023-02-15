4cf7561d546519068fbfb834f0016248e89bbc02
wushengjianzong <943459049@qq.com>
2023-02-16 00:28:26 +0800

    test: host url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/cit965.ico',
 
   // Set the production url of your site here
-  url: 'https://cit965.github.io/',
+  url: 'https://cit965.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/knowledge',
